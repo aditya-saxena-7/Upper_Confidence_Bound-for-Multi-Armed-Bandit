56400d0df9a9eafe8e057da79d1df8b84e03777b
aditya77777 <as9820502666@gmail.com>
2019-12-12 10:25:11 +0400

    UCB

---
+++ Upper_Bound_Confidence.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 09:07:29 2019
+
+@author: Aditya Saxena
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+
+dataset = pd .read_csv('Ads_CTR_Optimisation.csv')
+
+N = 10000
+d =10
+n= 10
+ads_selected = []
+number_of_selections = [0] * d
+sum_of_rewards = [0] * d
+total_rewards = 0
+
+for n in range(0,N):
+    ad = 0
+    max_upper_bound = 0
+    
+    for i in range(0,d):
+        if (number_of_selections[i] > 0):
+            average_reward = sum_of_rewards[i] / number_of_selections[i]
+            delta_i = math.sqrt(3/2 * math.log(n+1) / number_of_selections[i])
+            upper_bound = average_reward + delta_i
+        else:
+            upper_bound = 1e400
+        
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
+            
+    ads_selected.append(ad)
+    number_of_selections[ad] += 1
+    reward = dataset.values[n, ad]
+    sum_of_rewards[ad] += reward
+    total_rewards += reward
+     
+     
+     
+
+plt.hist(ads_selected)
+plt.title('Histogram of ads_selection')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
+
+
+
+     
+            
